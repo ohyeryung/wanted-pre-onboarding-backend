8a3b5fc102388249de25ed8d3e443a63ccd6fb8f
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-07-30 22:29:39 +0900

    :tada: begin a project

---
+++ src/main/java/com/smile/wanted_pre_task/WantedPreTaskApplication.java
@@ -0,0 +1,13 @@
+package com.smile.wanted_pre_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreTaskApplication.class, args);
+    }
+
+}
