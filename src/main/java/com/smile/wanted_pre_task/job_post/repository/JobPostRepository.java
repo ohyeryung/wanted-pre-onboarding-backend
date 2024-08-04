2f8d9eba2388047918aad6203e1260eac64566bc
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-04 19:27:33 +0900

    :recycle: refactor. 게시글 조회 시 시간 역순으로 조회

--- src/main/java/com/smile/wanted_pre_task/job_post/repository/JobPostRepository.java
+++ src/main/java/com/smile/wanted_pre_task/job_post/repository/JobPostRepository.java
@@ -3,5 +3,10 @@ package com.smile.wanted_pre_task.job_post.repository;
 import com.smile.wanted_pre_task.job_post.domain.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
+
+    List<JobPost> findAllByOrderByCreatedAtDesc();
+    List<JobPost> findAllByCompanyCompanyIdOrderByCreatedAtDesc (Long companyId);
 }
