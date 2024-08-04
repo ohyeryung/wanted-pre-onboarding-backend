8b473dda511a2a91653c97419a0e915fcb20f819
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-04 19:19:45 +0900

    :recycle: refactor. 게시글 수정 기능 변경

--- src/main/java/com/smile/wanted_pre_task/job_post/dto/JobPostDto.java
+++ src/main/java/com/smile/wanted_pre_task/job_post/dto/JobPostDto.java
@@ -1,11 +1,14 @@
 package com.smile.wanted_pre_task.job_post.dto;
 
 import com.smile.wanted_pre_task.company.domain.Company;
+import com.smile.wanted_pre_task.config.CalculateTime;
 import com.smile.wanted_pre_task.job_post.domain.JobPost;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 public class JobPostDto {
 
@@ -34,7 +37,6 @@ public class JobPostDto {
     }
 
     @Getter
-    @AllArgsConstructor
     @NoArgsConstructor
     public static class Response {
         private Long postId;
@@ -44,7 +46,22 @@ public class JobPostDto {
         private String position;
         private int reward;
         private String stack;
+        private String createdAt;
+        private String updatedAt;
+
+        public Response(Long postId, String companyName, String nation, String region, String position, int reward, String stack, LocalDateTime createdAt, LocalDateTime updatedAt) {
+            this.postId = postId;
+            this.companyName = companyName;
+            this.nation = nation;
+            this.region = region;
+            this.position = position;
+            this.reward = reward;
+            this.stack = stack;
+            this.createdAt = CalculateTime.dateformatForPost(createdAt);
+            this.updatedAt = CalculateTime.dateformatForPost(updatedAt);
+        }
     }
+
 
     @Getter
     @AllArgsConstructor
@@ -52,6 +69,7 @@ public class JobPostDto {
     public static class Update {
         private String title;
         private String position;
+        private String region;
         private int reward;
         private String content;
         private String stack;
@@ -62,6 +80,16 @@ public class JobPostDto {
             this.reward = jobPost.getReward();
             this.content = jobPost.getContent();
             this.stack = jobPost.getStack();
+        }
+
+        public JobPost toUpdateEntity() {
+            return JobPost.builder()
+                    .title(title)
+                    .position(position)
+                    .reward(reward)
+                    .content(content)
+                    .stack(stack)
+                    .build();
         }
     }
 }
