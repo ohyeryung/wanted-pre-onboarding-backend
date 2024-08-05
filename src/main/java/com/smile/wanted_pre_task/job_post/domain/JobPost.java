5da90ca88cc2485034098b34cfe991f95cb40964
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-06 00:40:32 +0900

    :recycle: refactor. 채용 공고와 회원 간의 관계 설정

--- src/main/java/com/smile/wanted_pre_task/job_post/domain/JobPost.java
+++ src/main/java/com/smile/wanted_pre_task/job_post/domain/JobPost.java
@@ -1,11 +1,14 @@
 package com.smile.wanted_pre_task.job_post.domain;
 
 import com.smile.wanted_pre_task.company.domain.Company;
-import com.smile.wanted_pre_task.config.Timestamped;
+import com.smile.wanted_pre_task.global.config.Timestamped;
+import com.smile.wanted_pre_task.member.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TB_JOBPOST")
@@ -17,7 +20,6 @@ public class JobPost extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "POST_ID")
     private Long postId;
-
     @Column(name = "TITLE")
     private String title;
     @Column(name = "POSITION")
@@ -31,8 +33,11 @@ public class JobPost extends Timestamped {
     @ManyToOne
     @JoinColumn(name = "COMPANY_ID")
     private Company company;
+    @Builder.Default
+    @OneToMany(mappedBy = "jobPost", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Member> members = new ArrayList<>();
 
-    public JobPost(Long postId, String title, String position, int reward, String content, String stack, Company company) {
+    public JobPost(Long postId, String title, String position, int reward, String content, String stack, Company company, List<Member> members) {
         this.postId = postId;
         this.title = title;
         this.position = position;
@@ -40,6 +45,7 @@ public class JobPost extends Timestamped {
         this.content = content;
         this.stack = stack;
         this.company = company;
+        this.members = members;
     }
 
     public JobPost() {
@@ -52,5 +58,10 @@ public class JobPost extends Timestamped {
         this.reward = jobPostUpdateReqDto.getReward();
         this.content = jobPostUpdateReqDto.getContent();
         this.stack = jobPostUpdateReqDto.getStack();
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+        member.setJobPost(this);
     }
 }
--- src/main/java/com/smile/wanted_pre_task/member/domain/Member.java
+++ src/main/java/com/smile/wanted_pre_task/member/domain/Member.java
@@ -1,9 +1,19 @@
 package com.smile.wanted_pre_task.member.domain;
 
+import com.smile.wanted_pre_task.job_post.domain.JobPost;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "TB_MEMBER")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -14,4 +24,12 @@ public class Member {
     private String memberEmail;
     @Column(name = "PASSWORD")
     private String password;
+    @ManyToOne
+    @JoinColumn(name = "POST_ID")
+    private JobPost jobPost;
+
+    public void setJobPost(JobPost jobPost) {
+        this.jobPost = jobPost;
+    }
+
 }
