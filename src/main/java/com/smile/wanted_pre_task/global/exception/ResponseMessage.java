99824262601f8bba5ef065ab4325480fe8b62189
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-06 01:22:55 +0900

    :safety_vest: feat. 채용 공고 중복 지원 시 발생하는 에러 메세지 추가

--- src/main/java/com/smile/wanted_pre_task/global/exception/ResponseMessage.java
+++ src/main/java/com/smile/wanted_pre_task/global/exception/ResponseMessage.java
@@ -1,8 +1,9 @@
-package com.smile.wanted_pre_task.global;
+package com.smile.wanted_pre_task.global.exception;
 
 public class ResponseMessage {
-    public static final String JOBPOST_NOT_FOUND = "채용 공고를 찾을 수 없습니다.";
+    public static final String JOB_POST_NOT_FOUND = "채용 공고를 찾을 수 없습니다.";
     public static final String MEMBER_NOT_FOUND = "회원을 찾을 수 없습니다";
     public static final String COMPANY_NOT_FOUND = "회사를 찾을 수 없습니다.";
     public static final String DELETE_SUCCESS = "성공적으로 삭제되었습니다.";
+    public static final String ALREADY_APPLIED = "이미 지원한 채용 공고입니다.";
 }
