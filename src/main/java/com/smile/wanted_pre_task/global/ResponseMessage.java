52f4e7a05cc19d7bb5e19a923fb0190b7290ad71
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-06 00:49:18 +0900

    :safety_vest: feat. 예외 처리 메세지 추가

---
+++ src/main/java/com/smile/wanted_pre_task/global/ResponseMessage.java
@@ -0,0 +1,8 @@
+package com.smile.wanted_pre_task.global;
+
+public class ResponseMessage {
+    public static final String JOBPOST_NOT_FOUND = "채용 공고를 찾을 수 없습니다.";
+    public static final String MEMBER_NOT_FOUND = "회원을 찾을 수 없습니다";
+    public static final String COMPANY_NOT_FOUND = "회사를 찾을 수 없습니다.";
+    public static final String DELETE_SUCCESS = "성공적으로 삭제되었습니다.";
+}
