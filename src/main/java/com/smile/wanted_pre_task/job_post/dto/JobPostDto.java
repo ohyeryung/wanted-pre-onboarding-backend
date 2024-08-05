368d2129607547338d6fc63f86d8876bb3d4ff57
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-06 00:50:09 +0900

    :safety_vest: feat. 유효성 검증 어노테이션 추가

--- src/main/java/com/smile/wanted_pre_task/job_post/dto/JobPostDto.java
+++ src/main/java/com/smile/wanted_pre_task/job_post/dto/JobPostDto.java
@@ -1,13 +1,16 @@
 package com.smile.wanted_pre_task.job_post.dto;
 
 import com.smile.wanted_pre_task.company.domain.Company;
-import com.smile.wanted_pre_task.config.CalculateTime;
+import com.smile.wanted_pre_task.global.config.CalculateTime;
 import com.smile.wanted_pre_task.job_post.domain.JobPost;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public class JobPostDto {
@@ -17,11 +20,18 @@ public class JobPostDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Post {
+        
+        @NotNull(message = "회사 ID는 필수 입력 값입니다.")
         private Long companyId;
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
         private String title;
+        @NotBlank(message = "채용 포지션은 필수 입력 값입니다.")
         private String position;
+
+        @NotNull(message = "채용 보상금은 필수 입력 값입니다.")
         private int reward;
         private String content;
+        @NotBlank(message = "사용기술은 필수 입력 값입니다.")
         private String stack;
 
         public JobPost toRegisterEntity(Company company) {
@@ -67,11 +77,15 @@ public class JobPostDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Update {
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
         private String title;
+        @NotBlank(message = "채용 포지션은 필수 입력 값입니다.")
         private String position;
-        private String region;
+
+        @NotNull(message = "채용 보상금은 필수 입력 값입니다.")
         private int reward;
         private String content;
+        @NotBlank(message = "사용기술은 필수 입력 값입니다.")
         private String stack;
 
         public Update(JobPost jobPost) {
@@ -90,6 +104,20 @@ public class JobPostDto {
                     .content(content)
                     .stack(stack)
                     .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Apply {
+        @NotNull(message = "채용 공고 ID는 필수 입력 값입니다.")
+        private Long postId;
+        @NotNull(message = "회원 ID는 필수 입력 값입니다.")
+        private Long memberId;
+
+        public Apply(Long postId, Long memberId) {
+            this.postId = postId;
+            this.memberId = memberId;
         }
     }
 }
