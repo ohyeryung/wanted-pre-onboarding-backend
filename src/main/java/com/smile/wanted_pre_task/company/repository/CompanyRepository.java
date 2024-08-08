488fcae88aede98aa09ada335ab4209b2d518f82
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-08 18:07:38 +0900

    :recycle: refactor. 회사, 회원 ID 타입 변경

--- src/main/java/com/smile/wanted_pre_task/company/repository/CompanyRepository.java
+++ src/main/java/com/smile/wanted_pre_task/company/repository/CompanyRepository.java
@@ -3,5 +3,7 @@ package com.smile.wanted_pre_task.company.repository;
 import com.smile.wanted_pre_task.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+import java.util.UUID;
+
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
 }
